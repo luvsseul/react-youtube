146989f5c1701372e432e277763644ecb6adcacb
luvsseul <150340509+luvsseul@users.noreply.github.com>
2024-01-08 15:07:50 +0900

    Use https

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -7,7 +7,7 @@ export default function VideoDetail() {
     const {title, channelId, channelTitle, description} = video.snippet;
     return (
         <section>
-            <iframe id="player" type="text/html" width="100%" height="640" src={`http://www.youtube.com/embed/${video.id}`} title={title} />
+            <iframe id="player" type="text/html" width="100%" height="640" src={`https://www.youtube.com/embed/${video.id}`} title={title} />
             <div className='p-8'>
                 <h2 className='text-xl font-bold'>{title}</h2>
                 <ChannelInfo id={channelId} name={channelTitle} />
